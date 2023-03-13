e6facbe3ac35eeb86ccfef7cf5ca48c795b8794c
Mukul V <97366719+mukulve@users.noreply.github.com>
2023-03-13 14:00:17 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -9,32 +9,40 @@ fn main() {
     let mut c_file = String::new();
     let mut make_file = String::new();
     let gcc_command = "gcc -Wall -std=c99 ";
+    let mut temp_str = String::new();
+    let stdin = io::stdin();
     //Get user input
     println!("Enter Main File ex: main.c : ");
-    io::stdin()
+    stdin
         .read_line(&mut main_file)
         .expect("Couldnt Read From STDIO");
     println!("Enter C Files seperated by spaces ex: 1.c 2.c : ");
-    io::stdin()
+    stdin
         .read_line(&mut c_file)
         .expect("Couldnt Read From STDIO");
     println!("Enter Header File ex: main.h : ");
-    io::stdin()
+    stdin
         .read_line(&mut header_file)
         .expect("Couldnt Read From STDIO");
     //Remove new line character from use input
-    main_file.truncate(main_file.len() - 1);
     header_file.truncate(header_file.len() - 1);
     c_file.truncate(c_file.len() - 1);
     //Create and Format makefile
     make_file.push_str("output: ");
     for x in c_file.split(" ") {
-        make_file.push_str(x.split(".c").next().unwrap_or(""));
-        make_file.push_str(".o ");
+        temp_str = format!("{}.o ", x.split(".c").next().unwrap_or(""));
+        make_file.push_str(&temp_str);
+        temp_str.clear();
     }
-    make_file.push_str(main_file.split(".c").next().unwrap_or(""));
-    make_file.push_str(".o\n\t");
-    make_file.push_str(gcc_command);
+
+    temp_str = format!(
+        "{}.o\n\t{}",
+        main_file.split(".c").next().unwrap_or(""),
+        gcc_command
+    );
+    make_file.push_str(&temp_str);
+    temp_str.clear();
+
     for x in c_file.split(" ") {
         make_file.push_str(x.split(".c").next().unwrap_or(""));
         make_file.push_str(".o ");
@@ -42,28 +50,30 @@ fn main() {
     make_file.push_str(&main_file);
     make_file.push_str(" -o output\n");
     for x in c_file.split(" ") {
-        make_file.push_str(x.split(".c").next().unwrap_or(""));
-        make_file.push_str(".o: ");
-        make_file.push_str(x);
-        make_file.push_str(" ");
-        make_file.push_str(&header_file);
-        make_file.push_str("\n\t");
-        make_file.push_str(gcc_command);
-        make_file.push_str("-c ");
-        make_file.push_str(x);
-        make_file.push('\n');
+        temp_str = format!(
+            "{}.o: {} {}\n\t{}-c {}\n",
+            x.split(".c").next().unwrap_or(""),
+            x,
+            &header_file,
+            gcc_command,
+            x
+        );
+
+        make_file.push_str(&temp_str);
+        temp_str.clear();
     }
-    make_file.push_str(main_file.split(".c").next().unwrap_or(""));
-    make_file.push_str(".o: ");
-    make_file.push_str(&main_file);
-    make_file.push_str(" ");
-    make_file.push_str(&header_file);
-    make_file.push_str("\n\t");
-    make_file.push_str(gcc_command);
-    make_file.push_str("-c ");
-    make_file.push_str(&main_file);
-    make_file.push('\n');
-    make_file.push_str("clean:\n\trm *.o output");
+
+    temp_str = format!(
+        "{}.o: {} {}\n\t{}-c {}\nclean:\n\trm *.o output",
+        main_file.split(".c").next().unwrap_or(""),
+        &main_file,
+        &header_file,
+        gcc_command,
+        &main_file
+    );
+    make_file.push_str(&temp_str);
+    temp_str.clear();
+
     //Show makefile onto console
     println!("----------");
     println!("{}", make_file);
